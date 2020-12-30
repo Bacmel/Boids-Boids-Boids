f8498de3381b457e254dd6f22e20b9adb35d4779
Bacmel <hugo.duarte91800@gmail.com>
2020-12-30 10:06:22 +0100

    Detection erreur Wall

--- src/canvas.py
+++ src/canvas.py
@@ -14,7 +14,7 @@ class Canvas:
         self.origin = border.origin
         self.shape = border.length
         self.render = render
-        self.ratio = np.max(np.multiply(self.res, 1/(self.shape+BOID_NOSE_LEN)))
+        self.ratio = np.min(np.multiply(self.res, 1/(self.shape+2*BOID_NOSE_LEN)))
 
         # A verifier
         self.current_frame = self.new_frame()
@@ -38,13 +38,13 @@ class Canvas:
         return self.shape
 
     def to_px(self, pos):
-        x = self.ratio*(pos[0]+self.shape[0]/2)
-        y = self.ratio*(-pos[1]+self.shape[1]/2)
+        x = self.ratio*(pos[0]+self.shape[0]/2 + BOID_NOSE_LEN)
+        y = self.ratio*(-pos[1]+self.shape[1]/2 + BOID_NOSE_LEN)
         return np.array([[x],[y]],dtype=int)
 
     def from_px(self, px):
         x = (px[0]/self.ratio)-self.shape[0]/2
-        y = (-pos[1]/self.ratio)-self.shape[1]/2
+        y = (-px[1]/self.ratio)-self.shape[1]/2
         return np.array([[x],[y]],dtype=float)
 
     def new_frame(self):
@@ -59,9 +59,12 @@ class Canvas:
         self.current_frame[:, :] = np.array(color, dtype="uint8")
 
     def draw_poly(self, points, color):
+        print("Liste des points : "+str(points))
+        px = [np.array([self.to_px(p).reshape(1,2) for p in points], dtype=np.int32)]
+        print("Liste des pixels : "+str(px))
         cv2.fillPoly(
             self.current_frame,
-            [np.array([self.to_px(p) for p in points], dtype=np.int32)],
+            px,
             # double list as fillPoly expects a list of polygons
             color,
             16,
