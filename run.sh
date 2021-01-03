8056b13701c3a139be9b1d2e076d6bac3f5b3e58
Florian Cormée <florian.cormee@gmail.com>
2021-01-03 12:31:06 +0100

    Use of blind spot & minor fix

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ python3.8 -m src \
 --border wrap \
 --view-dist 31 \
 -tr 50 -bv 5 \
---render \
 --error 0:0 \
 -ror 1 -roo 16 -roa 31 \
---step-nb 2000
+--blindspot-direction -180 --blindspot-opening 90 \
+--step-nb 200 --render
