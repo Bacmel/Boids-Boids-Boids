8b6e36345997df2aeeee22dbc3e317df69bf2e20
Florian Cormée <florian.cormee@gmail.com>
2021-01-09 11:38:11 +0100

    DataLogger cleaned up

--- src/data_logger.py
+++ src/data_logger.py
@@ -5,15 +5,20 @@ import datetime
 
 class DataLogger:
     def __init__(self):
-        super().__init__()
+        """Build a DataLogger with a default destination name."""
         now = datetime.datetime.now()
         name = now.strftime("%d-%m-%Y_%H-%M-%S")
         self.destination = f"logs/{name}/"
+        """str: The folder to save the CSV file in."""
         self.state = pandas.DataFrame()
+        """pandas.DataFrame: The state information."""
         self.quantities = pandas.DataFrame()
+        """pandas.DataFrame: The quantities information."""
 
     def flush(self):
+        """Flush all the data as CSV file."""
         self._mkdir_dest()
+
         # Store all data frame
         for name, value in self.__dict__.items():
             if isinstance(value, pandas.DataFrame):
@@ -21,8 +26,9 @@ class DataLogger:
                 value.to_csv(path)
 
     def _mkdir_dest(self):
+        """Make the required directories."""
         from_start = ""
-        for folder in self.destination.split('/')[:-1]:
+        for folder in self.destination.split("/")[:-1]:
             from_start += f"{folder}/"
             if not os.path.exists(from_start):
                 os.mkdir(from_start)
